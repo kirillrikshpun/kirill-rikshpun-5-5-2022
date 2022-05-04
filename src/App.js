a3dc5501d01bf8900cac19f6e338feb910681ae0
kirillrikshpun <kirillllrikshpun@gmail.com>
2022-05-04 17:06:35 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  Routes,
+  Route,
+} from 'react-router-dom'
+
+import { SnackbarProvider } from 'notistack';
+import Home from './Home/Home'
+import Favourites from './Favourites/Favourites';
+// import "../src/App.css"
+import NavBar from './NavBar/NavBar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SnackbarProvider maxSnack={3}>
+        <NavBar/>
+        <Routes>
+            <Route
+              index
+              element={<Home />}
+            />
+            <Route
+              path="favorite"
+              element={<Favourites />}
+            />
+        </Routes>
+    </SnackbarProvider>
+        
   );
 }
 
