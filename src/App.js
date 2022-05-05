fa6e807e7859067afb64f486a9878523d49ef23c
kirillrikshpun <kirillllrikshpun@gmail.com>
2022-05-05 12:00:13 +0300

    kirill-rikshpun-5-5-2022 was added to router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { SnackbarProvider } from "notistack";
-import Paper from "@mui/material/Paper"
+import Paper from "@mui/material/Paper";
 
 import Home from "./components/Home";
 import Favourites from "./components/Favourites";
 import NavBar from "./components/NavBar";
 
 function App() {
-  const [mode, setMode] = useState(true)
+  const [mode, setMode] = useState(true);
   const temperatureInC = (temp) => {
     const avarageF =
       (temp.Temperature.Maximum.Value + temp.Temperature.Minimum.Value) / 2;
@@ -31,11 +31,14 @@ function App() {
     <ThemeProvider theme={theme}>
       <SnackbarProvider maxSnack={3}>
         <Paper sx={{ height: "100vh" }}>
-          <NavBar mode = {mode} setMode={setMode} />
+          <NavBar mode={mode} setMode={setMode} />
           <Routes>
-            <Route index element={<Home temperatureInC={temperatureInC} />} />
             <Route
-              path="favorite"
+              path="kirill-rikshpun-5-5-2022/"
+              element={<Home temperatureInC={temperatureInC} />}
+            />
+            <Route
+              path="kirill-rikshpun-5-5-2022/favorite"
               element={<Favourites temperatureInC={temperatureInC} />}
             />
           </Routes>
