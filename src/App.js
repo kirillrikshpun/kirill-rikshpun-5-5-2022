875d438c71b2eb9bebdd759f4a302162414b056a
kirillrikshpun <kirillllrikshpun@gmail.com>
2022-05-05 11:07:15 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,32 +1,47 @@
-import React from 'react'
-import {
-  Routes,
-  Route,
-} from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { SnackbarProvider } from "notistack";
+import Paper from "@mui/material/Paper"
 
-import { SnackbarProvider } from 'notistack';
-import Home from './Home/Home'
-import Favourites from './Favourites/Favourites';
-// import "../src/App.css"
-import NavBar from './NavBar/NavBar';
-
+import Home from "./components/Home";
+import Favourites from "./components/Favourites";
+import NavBar from "./components/NavBar";
 
 function App() {
+  const [mode, setMode] = useState(true)
+  const temperatureInC = (temp) => {
+    const avarageF =
+      (temp.Temperature.Maximum.Value + temp.Temperature.Minimum.Value) / 2;
+    const avarageC = (avarageF - 32) / 1.8;
+    return Math.round(avarageC) + "°C";
+  };
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: mode ? "light" : "dark",
+        },
+      }),
+    [mode]
+  );
+
   return (
-    <SnackbarProvider maxSnack={3}>
-        <NavBar/>
-        <Routes>
-            <Route
-              index
-              element={<Home />}
-            />
+    <ThemeProvider theme={theme}>
+      <SnackbarProvider maxSnack={3}>
+        <Paper sx={{ height: "100vh" }}>
+          <NavBar mode = {mode} setMode={setMode} />
+          <Routes>
+            <Route index element={<Home temperatureInC={temperatureInC} />} />
             <Route
               path="favorite"
-              element={<Favourites />}
+              element={<Favourites temperatureInC={temperatureInC} />}
             />
-        </Routes>
-    </SnackbarProvider>
-        
+          </Routes>
+        </Paper>
+      </SnackbarProvider>
+    </ThemeProvider>
   );
 }
 
